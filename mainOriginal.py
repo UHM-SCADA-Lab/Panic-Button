6f99223c5a51a483cc84b6c583d24463a3c05141
brewerj3 <100174522+brewerj3@users.noreply.github.com>
2023-04-12 11:11:03 -1000

    testing getting the ip address into the python script

--- mainOriginal.py
+++ mainOriginal.py
@@ -3,29 +3,29 @@
 ##Date: 10-26-22
 ##Function: To create a functional panic button for UH's SCADA Lab
 
-import RPi.GPIO as GPIO     #import Raspberry PI Library
-import time                 #import sleep function for LED blinking
+import RPi.GPIO as GPIO  # import Raspberry PI Library
+import time  # import sleep function for LED blinking
 import os
 
-button = 10                 #Set variable for button pin
-led1 = 13                   #Set variable for led 1 pin
-led2 = 15                   #Set variable for led 2 pin
-apiOut = 29                 #Set variable for api panic status
-Status = False              #Set variable for status of panic button
+button = 10                                     # Set variable for button pin
+led1 = 13                                       # Set variable for led 1 pin
+led2 = 15                                       # Set variable for led 2 pin
+apiOut = 29                                     # Set variable for api panic status
+Status = False                                  # Set variable for status of panic button
 buttonStatus = False
 oldflash = time.time()
 out = True
 
-GPIO.setwarnings(False)     #Ignore Warnings
-GPIO.setmode(GPIO.BOARD)    #use physical pin numbering
-GPIO.setup(led1, GPIO.OUT, initial=GPIO.LOW)    #Setting pin 13 as an output for led initialize it off
-GPIO.setup(led2, GPIO.OUT, initial=GPIO.LOW)    #Setting pin 15 as an output for led2 initialize it off
-GPIO.setup(button, GPIO.IN)                     #Setting pin 10 as an input from button
-GPIO.setup(apiOut, GPIO.OUT, initial=GPIO.LOW)  #Setting pin 29 as output fro api panic status
+GPIO.setwarnings(False)                         # Ignore Warnings
+GPIO.setmode(GPIO.BOARD)                        # use physical pin numbering
+GPIO.setup(led1, GPIO.OUT, initial=GPIO.LOW)    # Setting pin 13 as an output for led initialize it off
+GPIO.setup(led2, GPIO.OUT, initial=GPIO.LOW)    # Setting pin 15 as an output for led2 initialize it off
+GPIO.setup(button, GPIO.IN)                     # Setting pin 10 as an input from button
+GPIO.setup(apiOut, GPIO.OUT, initial=GPIO.LOW)  # Setting pin 29 as output fro api panic status
 
-if __name__ == "__main__":
+if __name__ == "__mainOriginal__":
     try:
-        while True:                             #Runs forever
+        while True:  # Runs forever
 
             old_button_status = buttonStatus
             buttonStatus = GPIO.input(button)
@@ -38,15 +38,15 @@ if __name__ == "__main__":
                     GPIO.output(apiOut, Status)
 
             if not Status:
-                GPIO.output(led1,0)
-                GPIO.output(led2,0)
+                GPIO.output(led1, 0)
+                GPIO.output(led2, 0)
             elif Status:
                 flash = time.time()
-                #flash Leds
+                # flash Leds
                 if flash - oldflash > .5:
                     out = not out
                     oldflash = time.time()
-                    GPIO.output(led2,out)
-                    GPIO.output(led1,not out)
+                    GPIO.output(led2, out)
+                    GPIO.output(led1, not out)
     finally:
         GPIO.cleanup()
