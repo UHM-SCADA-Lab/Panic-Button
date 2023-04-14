f9f3d3e8bc8cfa397235a7b776e4c5731b523cf1
brewerj3 <100174522+brewerj3@users.noreply.github.com>
2023-04-13 14:48:53 -1000

    reformatted testSocket.py

--- testSocket.py
+++ testSocket.py
@@ -1,5 +1,7 @@
+# This script tests getting a IP address for use in the API
 import socket
-hostname=socket.gethostname()
-IPAddr=socket.gethostbyname(hostname)
-print("Your hostname is "+hostname)
-print("Your IP address is "+IPAddr)
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+print("Your hostname is " + hostname)
+print("Your IP address is " + IPAddr)
