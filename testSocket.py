63463aa6e2acc9f83efef6a80d20b6c5780bdcfe
brewerj3 <100174522+brewerj3@users.noreply.github.com>
2023-05-04 16:07:17 -1000

    Delete testSocket.py
    this is a test script and is not needed

--- testSocket.py
+++
@@ -1,7 +0,0 @@
-# This script tests getting a IP address for use in the API
-import socket
-
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
-print("Your hostname is " + hostname)
-print("Your IP address is " + IPAddr)
